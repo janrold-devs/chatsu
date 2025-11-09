912f230a0fc9cc480dfa164aa06322ca2904cf12
janrold-devs <janroldvandeguinto18@gmail.com>
2025-11-09 10:56:34 +0800

    message routes implemented

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,20 @@
 import express from "express";
+import {
+  getAllContacts,
+  getMessagesbyUserId,
+  sendMessage,
+  getChatPartners,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesbyUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;

6f701e8367cfbc16b3b767bcd0d83e5214583161
janrold-devs <janroldvandeguinto18@gmail.com>
2025-11-11 15:27:36 +0800

    Onlinr users implemented

--- frontend/src/components/ChatList.jsx
+++ frontend/src/components/ChatList.jsx
@@ -2,10 +2,11 @@ import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import UsersLoadingSkeleton from "./UsersLoadingSkeleton";
 import NoChatsFound from "./NoChatsFound";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ChatList() {
-  const { getMyChatPartners, chats, isUserLoading, setSelectedUser } =
-    useChatStore();
+  const { getMyChatPartners, chats, isUserLoading, setSelectedUser } = useChatStore();
+  const { onlineUsers } = useAuthStore();
 
   useEffect(() => {
     getMyChatPartners();
@@ -23,7 +24,7 @@ function ChatList() {
           onClick={() => setSelectedUser(chat)}
         >
           <div className="flex items-center gap-3">
-            <div className={`avatar online`}>
+            <div className={`avatar ${onlineUsers.includes(chat._id) ? "online" : "offline"}`}>
               <div className="size-12 rounded-full">
                 <img
                   src={chat.profilePic || "/avatar.png"}
